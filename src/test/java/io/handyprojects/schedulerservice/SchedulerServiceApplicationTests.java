6a62c5cf91542359c2fcfc5a74b7e377c013cce4
Saeed Mirzapour <saeedmirzapour71@gmail.com>
2019-11-25 15:11:30 +0330

    initial commit

---
+++ src/test/java/io/handyprojects/schedulerservice/SchedulerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.handyprojects.schedulerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
